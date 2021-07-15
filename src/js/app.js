6fff7d1151899546dedb4fe18959c5ca21b565d8
Coby Tamayo <acobster@gmail.com>
2021-07-15 12:19:37 -0700

    make default plugins part of lib

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,9 @@
-import { mountEditorInto } from './lib'
-import { keymap } from "prosemirror-keymap"
-import { undo, redo, history } from "prosemirror-history"
-import {baseKeymap, liftEmptyBlock, splitBlock, chainCommands, newlineInCode, createParagraphNear} from "prosemirror-commands"
+import { mountEditorInto, defaultPlugins } from './lib'
 
 import { schema } from "./schema"
 
 mountEditorInto(document.querySelector('.editable'), {
   schema,
-  plugins: [
-    history(),
-    // give custom mappings priority
-    keymap({
-      "Mod-z": undo,
-      "Mod-y": redo,
-      // "Enter": chainCommands(newlineInCode, createParagraphNear, liftEmptyBlock, splitBlock),
-      "Mod-0": splitBlock,
-      "Mod-1": liftEmptyBlock,
-      "Mod-2": createParagraphNear,
-      "Shift-Enter": () => console.log('Shift'),
-      "Mod-Enter": () => console.log('Mod'),
-      "Alt-Enter": () => console.log('Alt'),
-    }),
-    keymap(baseKeymap),
-  ]
+  plugins: defaultPlugins,
 });
 
--- src/js/lib.js
+++ src/js/lib.js
@@ -1,6 +1,26 @@
 import {DOMParser, Schema} from 'prosemirror-model'
 import {EditorState} from "prosemirror-state"
 import {EditorView} from "prosemirror-view"
+import { keymap } from "prosemirror-keymap"
+import { undo, redo, history } from "prosemirror-history"
+import {baseKeymap, liftEmptyBlock, splitBlock, chainCommands, newlineInCode, createParagraphNear} from "prosemirror-commands"
+
+export const defaultPlugins = [
+  history(),
+  // give custom mappings priority
+  keymap({
+    "Mod-z": undo,
+    "Mod-y": redo,
+    // "Enter": chainCommands(newlineInCode, createParagraphNear, liftEmptyBlock, splitBlock),
+    "Mod-0": splitBlock,
+    "Mod-1": liftEmptyBlock,
+    "Mod-2": createParagraphNear,
+    "Shift-Enter": () => console.log('Shift'),
+    "Mod-Enter": () => console.log('Mod'),
+    "Alt-Enter": () => console.log('Alt'),
+  }),
+  keymap(baseKeymap),
+]
 
 export function mountEditorInto(node, config) {
   const state = EditorState.create(Object.assign({}, {
