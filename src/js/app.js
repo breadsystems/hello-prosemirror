169d2b1e16d4191099e8e4d691020ec242cb0e9f
Coby Tamayo <acobster@gmail.com>
2022-03-08 21:10:54 -0800

    back-to-basics proof-of-concept

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,24 @@
-import { mountEditorInto, defaultPlugins, schemaFromPatterns } from './lib'
+import {EditorState} from "prosemirror-state"
+import {EditorView} from "prosemirror-view"
+import {Schema, DOMParser} from "prosemirror-model"
+import {schema} from "prosemirror-schema-basic"
+import {addListNodes} from "prosemirror-schema-list"
+import {exampleSetup} from "prosemirror-example-setup"
 
-mountEditorInto(document.querySelector('.editable'), {
-  schema: schemaFromPatterns([]),
-  plugins: defaultPlugins,
+const node = document.querySelector('.editable-container');
+
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+// create a schema with list support.
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks
 });
 
+const state = EditorState.create({
+  doc: DOMParser.fromSchema(mySchema).parse(node),
+  plugins: exampleSetup({schema: mySchema}),
+})
+
+new EditorView(node.parentNode, { state });
+
+node.parentNode.removeChild(node);
--- src/js/lib.js
+++ src/js/lib.js
@@ -1,39 +1,34 @@
-import {DOMParser, Schema} from 'prosemirror-model'
-import {EditorState} from "prosemirror-state"
-import {EditorView} from "prosemirror-view"
-import { keymap } from "prosemirror-keymap"
-import { undo, redo, history } from "prosemirror-history"
-import {baseKeymap, liftEmptyBlock, splitBlock, chainCommands, newlineInCode, createParagraphNear} from "prosemirror-commands"
-import { schema } from "./schema"
+//import { keymap } from "prosemirror-keymap"
+//import { undo, redo, history } from "prosemirror-history"
+//import {
+//  baseKeymap,
+//  liftEmptyBlock,
+//  splitBlock,
+//  chainCommands,
+//  newlineInCode,
+//  createParagraphNear
+//} from "prosemirror-commands"
+//import { schema } from "./schema"
+
+//export const defaultPlugins = [
+//  history(),
+//  // give custom mappings priority
+//  keymap({
+//    "Mod-z": undo,
+//    "Mod-y": redo,
+//    // "Enter": chainCommands(newlineInCode, createParagraphNear, liftEmptyBlock, splitBlock),
+//    "Mod-0": splitBlock,
+//    "Mod-1": liftEmptyBlock,
+//    "Mod-2": createParagraphNear,
+//    "Shift-Enter": () => console.log('Shift'),
+//    "Mod-Enter": () => console.log('Mod'),
+//    "Alt-Enter": () => console.log('Alt'),
+//  }),
+//  keymap(baseKeymap),
+//]
+
 
 // TODO
 export const schemaFromPatterns = (patterns) => {
   return schema
-}
-
-export const defaultPlugins = [
-  history(),
-  // give custom mappings priority
-  keymap({
-    "Mod-z": undo,
-    "Mod-y": redo,
-    // "Enter": chainCommands(newlineInCode, createParagraphNear, liftEmptyBlock, splitBlock),
-    "Mod-0": splitBlock,
-    "Mod-1": liftEmptyBlock,
-    "Mod-2": createParagraphNear,
-    "Shift-Enter": () => console.log('Shift'),
-    "Mod-Enter": () => console.log('Mod'),
-    "Alt-Enter": () => console.log('Alt'),
-  }),
-  keymap(baseKeymap),
-]
-
-export function mountEditorInto(node, config) {
-  const state = EditorState.create(Object.assign({}, {
-    doc: DOMParser.fromSchema(config.schema).parse(node),
-  }, config))
-
-  new EditorView(node.parentNode, {state})
-
-  node.parentNode.removeChild(node)
 }
